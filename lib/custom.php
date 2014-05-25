62b5868fc62bc28e708f9fad382d78c74a778809
Erik James Albaugh <erik@maintainweb.co>
2014-05-25 09:45:47 -0700

    Minor Home Styles Update

--- lib/custom.php
+++ lib/custom.php
@@ -7,3 +7,5 @@ require_once locate_template('/lib/admin-theme.php');          // Custom functio
 
 add_filter('the_field', 'do_shortcode');
 add_filter('get_field', 'do_shortcode');
+add_filter('the_sub_field', 'do_shortcode');
+add_filter('get_sub_field', 'do_shortcode');
