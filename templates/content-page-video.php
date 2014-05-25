964af931e8c247c6408585e934c63379b883e790
Erik James Albaugh <erik@maintainweb.co>
2014-05-25 12:41:30 -0700

    Template Updates

--- templates/content-page-video.php
+++ templates/content-page-video.php
@@ -1,5 +1,4 @@
 <?php while (have_posts()) : the_post(); ?>
   <?php get_template_part('templates/content', 'video-shortcode'); ?>
   <?php the_content(); ?>
-  <?php wp_link_pages(array('before' => '<nav class="pagination">', 'after' => '</nav>')); ?>
 <?php endwhile; ?>
