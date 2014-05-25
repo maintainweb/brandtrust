bd2c021869ab6dca27d21023e410f4a7548c1ee1
Erik James Albaugh <erik@maintainweb.co>
2014-05-25 12:41:20 -0700

    Right Align Footer

--- templates/footer.php
+++ templates/footer.php
@@ -1,7 +1,7 @@
 <footer class="content-info" role="contentinfo">
   <div class="container">
     <?php dynamic_sidebar('sidebar-footer'); ?>
-    <div class="copyright pull-right"><p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?></p></div>
+    <div class="copyright pull-right text-right"><p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?></p></div>
   </div>
 </footer>
 
