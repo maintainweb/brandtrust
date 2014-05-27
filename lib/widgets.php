6d3e8625811a0817147d468747b0d2e67f0d12a8
Erik James Albaugh <erik@maintainweb.co>
2014-05-27 05:27:26 -0700

    Remove Footer Widget Area

--- lib/widgets.php
+++ lib/widgets.php
@@ -13,15 +13,6 @@ function roots_widgets_init() {
     'after_title'   => '</h3>',
   ));
 
-  register_sidebar(array(
-    'name'          => __('Footer', 'roots'),
-    'id'            => 'sidebar-footer',
-    'before_widget' => '<section class="widget %1$s %2$s">',
-    'after_widget'  => '</section>',
-    'before_title'  => '<h3>',
-    'after_title'   => '</h3>',
-  ));
-
   // Widgets
   register_widget('Roots_Vcard_Widget');
 }
